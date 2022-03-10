b858f006aa5325531f1b15646e31f754dffb5577
KhaiLogarit <truongtrinhkhaidng@gmail.com>
2022-03-10 16:22:46 +0700

    16:22 10/03/2022

--- C1121G1 - TruongTrinhKhai/module_3/ss10_jsp_and_jstl/bai_tap/calculator_app/src/main/java/CalculatorServlet.java
+++ C1121G1 - TruongTrinhKhai/module_3/ss10_jsp_and_jstl/bai_tap/calculator_app/src/main/java/CalculatorServlet.java
@@ -1,3 +1,5 @@
+import model.Calculator;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
@@ -14,32 +16,22 @@ public class CalculatorServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setCharacterEncoding("UTF-8");
-        float firstOperant = Float.parseFloat(request.getParameter("first-operand"));
+        float firstOperand = Float.parseFloat(request.getParameter("first-operand"));
         String operator = request.getParameter("operator");
-        float secondOperant = Float.parseFloat(request.getParameter("second-operand"));
+        float secondOperand = Float.parseFloat(request.getParameter("second-operand"));
         float result = 0;
-        switch (operator) {
-            case "+":
-                result = firstOperant + secondOperant;
-                break;
-            case "-":
-                result = firstOperant - secondOperant;
-                break;
-            case "*":
-                result = firstOperant * secondOperant;
-                break;
-            case "/":
-                result = firstOperant / secondOperant;
-                break;
-        }
-
         PrintWriter writer = response.getWriter();
         writer.println("<html>");
         writer.println("<head>");
         writer.println("<meta charset=\"UTF-8\">");
         writer.println("</head>");
         writer.println("<h1>RESULT:</h1>");
-        writer.println(firstOperant + " " + operator + " " + secondOperant + " = " + result);
+        try {
+            result = Calculator.calculate(firstOperand, secondOperand, operator);
+            writer.println(firstOperand + " " + operator + " " + secondOperand + " = " + result);
+        } catch (Exception e) {
+            writer.println("Error: " + e.getMessage());
+        }
         writer.println("</html>");
     }
 }
